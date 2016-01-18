76cddf1677cce8d2dffae225a31db0e543564896
jasonten <chenten1@gmail.com>
2016-01-18 15:34:13 +0800

    加入程式碼
    加入程式碼，並且加入新的測試方法

--- HarryPotterShoppingCar/HarryPotterShoppingCar.cs
+++ HarryPotterShoppingCar/HarryPotterShoppingCar.cs
@@ -9,12 +9,65 @@ namespace HarryPotterShoppingCar
     {
         public HarryPotterShoppingCar()
         {
-                
+            var booklists = new List<BookList>()
+            {
+             new  BookList(){ BookName = "HarryPotter1",BookCount = 5}
+               , new BookList(){ BookName = "HarryPotter2", BookCount = 1 }
+               , new BookList(){ BookName = "HarryPotter3", BookCount = 3 }
+               , new BookList(){ BookName = "HarryPotter4", BookCount = 4 }
+               , new BookList(){ BookName = "HarryPotter5", BookCount = 2 }
+            };
+
+            int price = 1325;
+
+            double Total = ShoppingCar(booklists, price);
         }
 
-        public void ShoppingCar()
+        public double ShoppingCar(List<BookList> booklists, int price)
         {
+            double Total = 0;
+            int MaxCount = 0;
+            MaxCount = booklists.Max(x => x.BookCount);
 
+            while (MaxCount > 0)
+            {
+                int booksCount = 0;
+                for (int books = 0; books < 5; books++)
+                {
+                    if (booklists[books].BookCount > 0)
+                    {
+                        booksCount += 1;
+                        booklists[books].BookCount -= 1;
+                    }
+                }
+                if (booksCount > 0)
+                {
+                    switch (booksCount)
+                    {
+                        case 2:
+                            Total += booksCount * price * 0.95;
+                            break;
+
+                        case 3:
+                            Total += booksCount * price * 0.9;
+                            break;
+
+                        case 4:
+                            Total += booksCount * price * 0.8;
+                            break;
+
+                        case 5:
+                            Total += booksCount * price * 0.75;
+                            break;
+
+                        default:
+                            Total += price;
+                            break;
+                    }
+                }
+                MaxCount -= 1;
+            }
+            return Total;
         }
     }
     public class BookList
--- HarryPotterShoppingCarTests/HarryPotterShoppingCarTests.cs
+++ HarryPotterShoppingCarTests/HarryPotterShoppingCarTests.cs
@@ -1,18 +1,15 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using HarryPotterShoppingCar;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 namespace HarryPotterShoppingCar.Tests
 {
     [TestClass()]
     public class HarryPotterShoppingCarTests
     {
         [TestMethod()]
-        public void HarryPotterShoppingCarTest()
+        public void HarryPotterShoppingCarTest_buy_BookName_1_BookCount_1_Total_100()
         {
+
             List<BookList> BookLists = new List<BookList>()
             {
              new  BookList(){ BookName = "HarryPotter1",BookCount = 1}
@@ -25,6 +22,27 @@ namespace HarryPotterShoppingCar.Tests
             var actual = BookLists[0].BookCount * price;
 
             //assert
+            Assert.AreEqual(Total, actual);
+        }
+
+        [TestMethod()]
+        public void HarryPotterShoppingCarTests_buy_Book1_5_Book2_1_Book3_4_Book4_4_Book5_2_Total_1335()
+        {
+             var target = new HarryPotterShoppingCar();
+            List<BookList> BookLists = new List<BookList>()
+            {
+             new  BookList(){ BookName = "HarryPotter1",BookCount = 5}
+             , new  BookList(){ BookName = "HarryPotter2",BookCount = 1}
+             , new  BookList(){ BookName = "HarryPotter3",BookCount = 4}
+             ,  new  BookList(){ BookName = "HarryPotter4",BookCount = 4}
+             , new  BookList(){ BookName = "HarryPotter5",BookCount = 2}
+            };
+            int price = 100;
+
+            var Total = 1335;
+
+            var actual = target.ShoppingCar(BookLists, price);
+
             Assert.AreEqual(Total, actual);
         }
     }
