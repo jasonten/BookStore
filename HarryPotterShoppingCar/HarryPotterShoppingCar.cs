12e8633b40fd7dff724d8e23b4dab86676ed4777
jasonten <chenten1@gmail.com>
2016-01-18 14:35:37 +0800

    建立專案
    第一次只買一本單一書籍基本測試

---
+++ HarryPotterShoppingCar/HarryPotterShoppingCar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HarryPotterShoppingCar
+{
+    public class HarryPotterShoppingCar
+    {
+        public HarryPotterShoppingCar()
+        {
+                
+        }
+
+        public void ShoppingCar()
+        {
+
+        }
+    }
+    public class BookList
+    {
+        public string BookName { get; set; }
+        public int BookCount { get; set; }
+    }
+}
---
+++ HarryPotterShoppingCarTests/HarryPotterShoppingCarTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarryPotterShoppingCar;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace HarryPotterShoppingCar.Tests
+{
+    [TestClass()]
+    public class HarryPotterShoppingCarTests
+    {
+        [TestMethod()]
+        public void HarryPotterShoppingCarTest()
+        {
+            List<BookList> BookLists = new List<BookList>()
+            {
+             new  BookList(){ BookName = "HarryPotter1",BookCount = 1}
+            };
+            int price = 100;
+
+            var Total = 100;
+
+            //act
+            var actual = BookLists[0].BookCount * price;
+
+            //assert
+            Assert.AreEqual(Total, actual);
+        }
+    }
+}
